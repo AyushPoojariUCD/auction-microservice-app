4bf4ae24da6ed251583b121a9a8541c76d44261b
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-08-23 15:26:28 +0530

    feat: added API Endpoint via SearchController

--- src/SearchService/Controllers/SearchConroller.cs
+++ src/SearchService/Controllers/SearchConroller.cs
@@ -9,23 +9,33 @@ namespace SearchService.Controllers;
 [Route("api/search")]
 public class SearchController : ControllerBase
 {
-    [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
-    {
-        var query = DB.Find<Item>();
 
-        // Default sort by Make
+    [HttpGet]
+    public async Task<ActionResult> SearchItems(
+        [FromQuery] string? searchTerm = null,
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 4)
+    {
+        var query = DB.PagedSearch<Item>();
+
         query.Sort(x => x.Ascending(a => a.Make));
 
-        // If search term exists, do full-text search and sort by text score
         if (!string.IsNullOrEmpty(searchTerm))
         {
-            query.Match(Search.Full, searchTerm)
-                 .SortByTextScore();
+            query.Match(Search.Full, searchTerm).SortByTextScore();
         }
+        
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
 
         var result = await query.ExecuteAsync();
 
-        return Ok(result); 
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
+
