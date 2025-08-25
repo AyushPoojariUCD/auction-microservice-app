b264cd1c9a48d952fecee784888781d82dab18d4
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-08-25 08:12:18 +0530

    feat: updated RequestHelpers

--- src/SearchService/RequestHelpers/SearchParams.cs
+++ src/SearchService/RequestHelpers/SearchParams.cs
@@ -1,24 +1,12 @@
-namespace SearchService.RequestHelpers
-{
-    public class SearchParams
-    {
-        public string? SearchTerm { get; set; }
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 4;
-        
-        // Specific filters
-        public string? Seller { get; set; }
-        public string? Winner { get; set; }
-        public string? Status { get; set; }
-        
-        // Sorting & high-level filtering
-        public string? OrderBy { get; set; }
-        public string? FilterBy { get; set; }
+namespace SearchService;
 
-        // Optional numeric filters
-        public int? MinReservePrice { get; set; }
-        public int? MaxReservePrice { get; set; }
-        public int? MinBid { get; set; }
-        public int? MaxBid { get; set; }
-    }
+public class SearchParams
+{
+    public string SearchTerm { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 4;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public string OrderBy { get; set; }
+    public string FilterBy { get; set; }
 }
