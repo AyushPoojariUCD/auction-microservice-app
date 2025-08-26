dd95c19c4a0b1eb48a9f1acae90508fc7297f7f3
AyushPoojariUCD <ayushpoojari@ucdconnect.ie>
2025-08-27 01:56:25 +0530

    feat: updated aution-service

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,7 +1,9 @@
 using MassTransit;
+using AuctionService;
 using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
-using AuctionService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,9 +41,22 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
+// Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+});
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 try
