20801f8531c75d12a7886343b08342f206e8eb5a
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-08-27 13:40:48 +0530

    feat: implemented gateway-service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
